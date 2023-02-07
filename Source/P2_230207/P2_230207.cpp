7b1e744c2214acfe7841644e9cfd252b5ac45c83
Kim <akk0425@naver.com>
2023-02-07 17:39:59 +0900

    Create Source for C++  - MyGameModeBase  - MyPlayerController  - MyPawn

---
+++ Source/P2_230207/P2_230207.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "P2_230207.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P2_230207, "P2_230207" );
